ba76203b48a29415f49c563059167dd64a28dab2
Zachary Allwein <86269947+ZacharyAllwein@users.noreply.github.com>
2022-03-30 14:31:40 -0500

    going to work on some client things next maybe?

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,38 +1,28 @@
-use serde_json::{json, to_string_pretty};
-use std::error::{self, Error};
-use std::fs::{self, File};
-use std::io;
+use serde_json;
+use std::fs;
 use std::process;
+use std::path::Path;
 
 fn main() {
     
-    let config = match get_config(){
-        Ok(conf) => conf,
-        Err(e) => {
-            eprintln!("Config fetch error: {}", e);
-            process::exit(1);
+    let config: serde_json::Value = {
+        if Path::new("ytc_config").exists() {
+            let file = fs::read_to_string("ytc_config").unwrap();
+            serde_json::from_str(&file).expect("Malformed Json")
         }
-    };
-}
-
-fn get_config() -> Result<serde_json::Value, Box<dyn Error>> {
-
-    //attempt to read in config file if it doesn't exist, create a new one and exit
-    let file = fs::read_to_string("ytc_config.json").or_else(|err| {
-        if err.kind() == io::ErrorKind::NotFound {
-            let config_json = json!({
+        else {
+            let config_json = serde_json::json!({
                 "read": false,
                 "port": 7777,
             });
-            fs::write("ytc_config.json", to_string_pretty(&config_json).unwrap())?;
-            process::exit(1);
-        } else {
-            return Err(err);
+
+            fs::write("ytc_config.json", serde_json::to_string_pretty(&config_json).unwrap()).unwrap();
+            config_json
         }
-    })?;
+    };
 
-    //read in config file as serde Value
-    let config: serde_json::Value = serde_json::from_str(&file)?;
-
-    Ok(config)
+    if !&config["read"].as_bool().unwrap(){
+        eprintln!("Please set 'read' in config to true");
+        process::exit(1);
+    }
 }
